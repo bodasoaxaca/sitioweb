c3b7f0e6d1ae06ab9fe6f7742064d83fb4667552
Miguel Palau <mpalauzarza@gmail.com>
2015-11-09 14:01:46 -0600

    se integra hideable menu

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,17 @@ function mobileNav() {
 }
 
 mobileNav();
+
+function hideNav() {
+  var prev = 0;
+  var $window = $(window);
+  var nav = $('header');
+
+  $window.on('scroll', function(){
+    var scrollTop = $window.scrollTop();
+    nav.toggleClass('hidden', scrollTop > prev);
+    prev = scrollTop;
+  });
+}
+
+hideNav();
