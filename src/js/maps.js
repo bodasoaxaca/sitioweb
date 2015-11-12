3bebda7c780af3479592b100062d6b1af83737c8
Miguel Palau <mpalauzarza@gmail.com>
2015-11-12 14:58:03 -0600

    se agrega pagina de nosotros

---
+++ src/js/maps.js
@@ -0,0 +1,19 @@
+new Maplace({
+
+    locations: [{
+        lat: 17.036088,
+        lon: -96.713684,
+        zoom: 18,
+        title : 'Bodas Oaxaca',
+        html : '<h3>Bodas Oaxaca</h3> <p>Fresnito, 206 Local 3 Col. Ex-Hacienda Candiani 71233 Oaxaca, Mexico</p>'
+    }],
+    map_options : {
+								scrollwheel : false,
+								mapTypeControl : false,
+								zoomControlOptions : {
+									style : google.maps.ZoomControlStyle.SMALL
+								        }
+							  }
+
+  })
+  .Load();
