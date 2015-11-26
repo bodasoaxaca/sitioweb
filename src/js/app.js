6f58f2a45b93b7b382a4f576c74daf475a683f10
Miguel Palau <mpalauzarza@gmail.com>
2015-11-26 14:20:21 -0600

    listo instagram

--- src/js/app.js
+++ src/js/app.js
@@ -21,3 +21,45 @@ function hideNav() {
 }
 
 hideNav();
+
+
+function instaPics () {
+  // Seleccionamos el div donde vamos a pinches trabajar
+  var insta = $('#instagram');
+  var html = '';
+  // Definimos el URL del API de instagram
+  var url = 'https://api.instagram.com/v1/users/1252174431/media/recent?access_token=1252174431.5b9e1e6.3179e7e2c00b44ca86901c63785546f0';
+
+  // Este es el loop que va a construir todo el HTML con las fotos y así, mu.
+  function loop (fotos) {
+    for (var i = 0; i<12; i++) {
+      var fotoActual = fotos.data[i];
+      html+= '<div class="instagram__foto">'
+      + '<a class="instagram__foto--link" href="'
+      + fotoActual.link
+      + '" target="_blank">'
+      + '<img src="'
+      + fotoActual.images.standard_resolution.url
+      + '" alt="'
+      + fotoActual.caption.text
+      + '">'
+      + '</<a>'
+      + '<div class="instagram__foto--descripcion">'
+      + '<span>'
+      + fotoActual.caption.text
+      + '</span>'
+      +'</div></div>';
+    }
+    insta.html(html);
+  }
+
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    crossDomain: true,
+    success: loop
+  });
+
+}
+
+instaPics();
